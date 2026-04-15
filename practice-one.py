f5fa4195a7d8b7aaa021f4e7025acd028317e204
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-04-15 22:33:29 +0530

    Just Practicing

---
+++ practice-one.py
@@ -0,0 +1,8 @@
+scores = [80.5, 90.0, 88.6, 75.9]
+
+for score in scores:
+    if score == 90.0:
+        print(f"Outstanding, your mark is {score}")
+    else:
+        print("You need improvement")
+
